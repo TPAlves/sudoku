2a1d4a7a162dfd66fb6a4529bdf454a6c0b8a7e5
Thiago Pereira Alves <tpereiraalves2013@outlook.com>
2023-11-10 20:19:01 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<h3>Las Vegas Algorithm para resolver o jogo do Sudoku.</h3> 
+
+<p>O algoritmo gera uma solução válida para o Sudoku preenchendo 
+a grade de forma aleatória e verificando se a solução é válida. 
+Se a solução não for válida, o algoritmo tenta novamente 
+até encontrar uma solução válida.</p>
+
+<p>Projeto feito com a API dosuku: <a href="https://sudoku-api.vercel.app/">link para a documentação</a></p>
+
---
+++ main.py
@@ -0,0 +1,82 @@
+import random,requests, json;
+
+
+def eValido(sudoku, linha, coluna, numero):
+    # Verefica se o numero já existe na linha ou coluna
+    for x in range(9):
+        if sudoku[linha][x] == numero or sudoku[x][coluna] == numero:
+            return False;
+    
+    # Verifica se o número existe no 3x3
+    linha_inicial = linha - linha % 3;
+    coluna_inicial = coluna - coluna % 3;
+    
+    for i in range(3):
+        for j in range(3):
+            if sudoku[i + linha_inicial][j + coluna_inicial] == numero:
+                return False;
+    
+    return True;
+
+def resolve_sudoku(sudoku, linha=0, coluna=0):
+    if linha == 9: # Se chegou a última linha, o sudoku está resolvido
+        return True;
+    
+    if coluna == 9:
+        return resolve_sudoku(sudoku,linha + 1, 0);
+    
+    if sudoku[linha][coluna] != 0: # Se a célula já está preenchida, passamos para a próxima
+        return resolve_sudoku(sudoku,linha, coluna +1);
+    
+    for numero in random.sample(range(1,10),9): # Testando números de 1 a 9 em ordem aleatória 
+        if eValido(sudoku,linha,coluna,numero):
+            sudoku[linha][coluna] = numero;
+
+            if resolve_sudoku(sudoku,linha,coluna + 1):
+                return True;
+        
+        sudoku[linha][coluna] = 0
+        
+    return False;
+
+
+def conexao_api():
+    request = requests.get("https://sudoku-api.vercel.app/api/dosuku");
+    data = json.loads(request.content);
+    return data;
+
+def gera_sudoku():
+    data = conexao_api();
+    sudoku_api = data['newboard']['grids'][0]['value'];
+    dificuldade = data['newboard']['grids'][0]['difficulty'];
+    return sudoku_api, dificuldade;
+
+
+sudoku, dificuldade = gera_sudoku(); 
+
+print(""" 
+█████████                 █████          █████                   
+███░░░░░███               ░░███          ░░███                    
+░███    ░░░  █████ ████  ███████   ██████  ░███ █████ █████ ████   
+░░█████████ ░░███ ░███  ███░░███  ███░░███ ░███░░███ ░░███ ░███    
+░░░░░░░░███ ░███ ░███ ░███ ░███ ░███ ░███ ░██████░   ░███ ░███    
+███    ░███ ░███ ░███ ░███ ░███ ░███ ░███ ░███░░███  ░███ ░███    
+░░█████████  ░░████████░░████████░░██████  ████ █████ ░░████████""" + "\n\n" + """
+██████████ ██████████ ██████████ ██████████ ██████████ ██████████
+""")
+
+print(f"Dificuldade: {dificuldade}\n");
+
+print("Antes da solução: ");
+
+for linha in sudoku:
+    print(linha);
+    
+print("\n-------------------------\n");
+
+print("Solucionado: ");
+
+resolve_sudoku(sudoku);
+for linha in sudoku:
+    print(linha);
+
